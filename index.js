011315d00c34cea46b92d30340cefbd02f52ec42
Sam Bonfante <6620407+X-sam@users.noreply.github.com>
2020-10-03 10:55:45 -0400

    Use modern flow features.

--- index.js
+++ index.js
@@ -8,60 +8,61 @@ app.get('/',(req,res)=>{
 	res.status(404).send('');
 });
 
-const gethead = (user,repo,branch,res,cb)=>{
-    if(branch==='master') return 'HEAD';
+const gethead = async (user,repo,branch)=>{
+  if(branch==='master') return 'HEAD';
 	let url = 'https://api.github.com/repos/'+user+'/'+repo+'/branches/'+branch;
 	console.log("requesting "+url);
-    request
-        .get(url)
-        .set('User-Agent','request')
-        .set('Authorization','token '+config.OAuth)
-        .end(function(err,response){
-            if(err||!response.ok){
-                console.log('error:' +err);
-                console.log(response.text);
-                cb(user,repo,'',res);
-            }
-            else {
-                console.log('Got ' + response.body.commit.sha);
-                cb(user, repo, response.body.commit.sha, res);
-            }
-        });
+  try {
+	  const res = await request
+	    .get(url)
+      .set('User-Agent','request')
+      .set('Authorization','token '+config.OAuth)
+    if(!res.ok)
+      throw;
+    console.log('Got ' + res.body.commit.sha);
+    return res.body.commit.sha;
+  } catch(err) {
+    console.log(`error: ${err}`);
+    return '';
+  }
 };
 
-const getstatus = (user,repo,hash,res)=>{
-    let url = 'https://api.github.com/repos/'+user+'/'+repo+'/commits/'+hash+'/status';
-    console.log('requesting '+url);
-    request
-        .get(url)
-        .set('User-Agent','request')
-        .set('Authorization','token '+config.OAuth)
-        .end(function(error,response) {
-            if (error || !response.ok) {
-                console.log('status: ' + response.statusCode);
-                console.log('body: ' + response.text);
-                request.get('https://img.shields.io/badge/build-status unknown-blue.svg').pipe(res);
-            }
-            else {
-                let s = response.body.state;
-                if (s === 'success')
-                    request.get('https://img.shields.io/badge/build-passing-green.svg').pipe(res);
-                else if (s === 'pending')
-                    request.get('https://img.shields.io/badge/build-pending-yellow.svg').pipe(res);
-                else if (s === 'failure')
-                    request.get('https://img.shields.io/badge/build-failing-red.svg').pipe(res);
-                else
-                    request.get('https://img.shields.io/badge/build-status unknown-blue.svg').pipe(res);
-            }
-        });
+const getstatus = async (user,repo,hash)=>{
+  const url = `https://api.github.com/repos/${user}/${repo}/commits/${hash}/status`;
+  console.log('requesting '+url);
+  try {
+    const res = await request
+      .get(url)
+      .set('User-Agent','request')
+      .set('Authorization','token '+config.OAuth)
+    if (!res.ok) {
+      throw;
+    }
+    switch(response.body.state){
+      case 'success':
+        return request.get('https://img.shields.io/badge/build-passing-green.svg');
+      case 'pending':
+        return request.get('https://img.shields.io/badge/build-pending-yellow.svg');
+      case 'failure':
+        return request.get('https://img.shields.io/badge/build-failing-red.svg');
+      default:
+        return request.get('https://img.shields.io/badge/build-status unknown-blue.svg');
+    }
+  } catch (err){
+    console.log(`err: ${err}`);
+    return request.get('https://img.shields.io/badge/build-status unknown-blue.svg');
+  };
 }
 
-app.get('/api/:user/:repo/:branch/:commit?',(req,res)=>{
+app.get('/api/:user/:repo/:branch/:commit?',async (req,res)=>{
     if(req.params.commit && req.params.commit !=='') {
     	console.log('commit: '+req.params.commit);
-	    getstatus(req.params.user,req.params.repo,req.params.commit,res);
+	    const rtn = await getstatus(req.params.user,req.params.repo,req.params.commit);
+      res.send(rtn);
     } else {
-	    gethead(req.params.user,req.params.repo,req.params.branch,res,getstatus);
+	    const sha = await gethead(req.params.user,req.params.repo,req.params.branch);
+      const rtn = await getstatus(req.params.user,req.params.repo,sha);
+      res.send(rtn);
     }
 });
 
